b93103a66aa129e35d395872dc0a89bb8b0e9648
test1564559 <testsolutions1967@gmail.com>
2023-09-04 12:32:38 +0200

    aanpassingen kleur en grote

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+//const expandButton = document.querySelector('header button')// header button
+const expandButton = document.querySelector('button')
+expandButton.addEventListener('click', expand)//klikken open gaan
 
 function expand () {
   document.body.classList.toggle('expand')
